c425680ea27028b487fbb691407b559d118361b6
michaelarnouse <michael.arnouse@richmond.edu>
2024-04-15 17:00:56 -0400

    patching item bids lower than start bid

--- routes/item-routes.js
+++ routes/item-routes.js
@@ -9,9 +9,9 @@ const upload = multer({ storage: storage });
 router
   .route("/")
   .get(itemController.getItems)
-  .post(authorize, upload.single("image"), itemController.postItem);
-// .patch(itemController.updateItemsHighestBid);
-// .patch(itemController.deleteBidsLowerThanStartBid);
+  .post(authorize, upload.single("image"), itemController.postItem)
+  // .patch(itemController.updateItemsHighestBid);
+  .patch(itemController.deleteBidsLowerThanStartBid);
 
 router
   .route("/:itemId")
